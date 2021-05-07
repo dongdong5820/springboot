a525368b791e8cc6bfdded8b37aa6915e4b4becb
ranlay.su <ranlay.su@oneplus.com>
2021-05-07 10:22:10 +0800

    banner测试

--- src/main/java/com/ranlay/Controller/IndexController.java
+++ src/main/java/com/ranlay/Controller/IndexController.java
@@ -12,9 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class IndexController {
-    // 定义请求路径为： localhost:8080/hello, 下面是对应这个路径的处理方法
-    // RequestParam用于接收页面传递过来的参数，例如 localhost:8080/hello?name=小明
-    // model是spring内置的对象，用来处理视图
+    /**
+     * 定义请求路径为： localhost:8080/hello, 下面是对应这个路径的处理方法
+     * RequestParam 用于接收页面传递过来的参数，例如 localhost:8080/hello?name=小明
+     * model 是spring内置的对象，用来处理视图
+     * @param name
+     * @param model
+     * @return
+     */
     @GetMapping("/hello")
     public String hello(@RequestParam(name="name") String name, Model model) {
         // 将浏览器的请求参数中的name加入到model中，这样就能在模板的html中获取到
