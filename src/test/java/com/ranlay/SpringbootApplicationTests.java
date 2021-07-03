344f905f5e62f29ed0487f1dd04eb437aa0cbd7a
dongdong5820 <407591245@qq.com>
2021-07-03 19:25:09 +0800

    springboot整合JDBC

--- src/test/java/com/ranlay/SpringbootApplicationTests.java
+++ src/test/java/com/ranlay/SpringbootApplicationTests.java
@@ -1,13 +1,26 @@
 package com.ranlay;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class SpringbootApplicationTests {
+    @Autowired
+    DataSource dataSource;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        // com.zaxxer.hikari.HikariDataSource
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        // HikariProxyConnection@170050776 wrapping com.mysql.cj.jdbc.ConnectionImpl@45cd7bc5
+        System.out.println(connection);
+        connection.close();
     }
 
 }
